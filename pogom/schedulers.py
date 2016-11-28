657cb3452588dbb33a33d22c6fd3c52d5f900ae4
Gonjiak <gonjiak@gmail.com>
2016-11-28 20:59:38 +0100

    Update Elevation Request
    Attempt to receive altitude via Elevation

--- pogom/schedulers.py
+++ pogom/schedulers.py
@@ -40,14 +40,14 @@ import math
 import geopy
 import json
 import random
+import requests
 from queue import Empty
 from operator import itemgetter
 from .transform import get_new_coords
 from .models import hex_bounds, Pokemon
-from .utils import now, cur_sec, get_args
+from .utils import now, cur_sec
 
 log = logging.getLogger(__name__)
-args = get_args()
 
 # Simple base class that all other schedulers inherit from.
 # Most of these functions should be overridden in the actual scheduler classes.
@@ -94,6 +94,8 @@ class BaseScheduler(object):
 
 # Hex Search is the classic search method, with the pokepath modification, searching in a hex grid around the center location.
 class HexSearch(BaseScheduler):
+    elevation = False
+    altitude = 0
 
     # Call base initialization, set step_distance.
     def __init__(self, queues, status, args):
@@ -106,7 +108,9 @@ class HexSearch(BaseScheduler):
             self.step_distance = 0.070
 
         self.step_limit = args.step_limit
-
+        self.gmaps = args.gmaps_key
+        self.altitude_range = args.altitude_range
+        self.altitude_default = args.altitude
         # This will hold the list of locations to scan so it can be reused, instead of recalculating on each loop.
         self.locations = False
 
@@ -201,10 +205,22 @@ class HexSearch(BaseScheduler):
         # Add the required appear and disappear times.
         locationsZeroed = []
         for step, location in enumerate(results, 1):
-            if args.altitude_range > 0:
-                altitude = args.altitude + random.randrange(-1 * args.altitude_range, args.altitude_range) + float(format(random.random(), '.13f'))
+            if HexSearch.elevation:
+                altitude = HexSearch.altitude
             else:
-                altitude = args.altitude + float(format(random.random(), '.13f'))
+                try:
+                    r_session = requests.Session()
+                    response = r_session.get("https://maps.googleapis.com/maps/api/elevation/json?locations={},{}&key={}".format(location[0], location[1], self.gmaps))
+                    response = response.json()
+                    altitude = response["results"][0]["elevation"]
+                    HexSearch.elevation = True
+                    HexSearch.altitude = altitude
+                except:
+                    altitude = self.altitude_default
+            if self.altitude_range > 0:
+                altitude = altitude + random.randrange(-1 * self.altitude_range, self.altitude_range) + float(format(random.random(), '.13f'))
+            else:
+                altitude = altitude + float(format(random.random(), '.13f'))
 
             locationsZeroed.append((step, (location[0], location[1], altitude), 0, 0))
         return locationsZeroed
@@ -250,6 +266,9 @@ class HexSearchSpawnpoint(HexSearch):
 
 # Spawn Scan searches known spawnpoints at the specific time they spawn.
 class SpawnScan(BaseScheduler):
+    elevation = False
+    altitude = 0
+
     def __init__(self, queues, status, args):
         BaseScheduler.__init__(self, queues, status, args)
         # On the first scan, we want to search the last 15 minutes worth of spawns to get existing
@@ -264,8 +283,11 @@ class SpawnScan(BaseScheduler):
 
         self.step_limit = args.step_limit
         self.locations = False
-
+        self.gmaps = args.gmaps_key
+        self.altitude_range = args.altitude_range
+        self.altitude_default = args.altitude
     # Generate locations is called when the locations list is cleared - the first time it scans or after a location change.
+
     def _generate_locations(self):
         # Attempt to load spawns from file.
         if self.args.spawnpoint_scanning != 'nofile':
@@ -340,11 +362,22 @@ class SpawnScan(BaseScheduler):
         # locations = [((lat, lng, alt), ts_appears, ts_leaves),...]
         retset = []
         for step, location in enumerate(self.locations, 1):
-            if args.altitude_range > 0:
-                altitude = args.altitude + random.randrange(- 1 * args.altitude_range, args.altitude_range) + float(format(random.random(), '.13f'))
+            if SpawnScan.elevation:
+                altitude = SpawnScan.altitude
             else:
-                altitude = args.altitude + float(format(random.random(), '.13f'))
-
+                try:
+                    r_session = requests.Session()
+                    response = r_session.get("https://maps.googleapis.com/maps/api/elevation/json?locations={},{}&key={}".format(location['lat'], location['lng'], self.gmaps))
+                    response = response.json()
+                    altitude = response["results"][0]["elevation"]
+                    SpawnScan.elevation = True
+                    SpawnScan.altitude = altitude
+                except:
+                    altitude = self.altitude_default
+            if self.altitude_range > 0:
+                altitude = altitude + random.randrange(-1 * self.altitude_range, self.altitude_range) + float(format(random.random(), '.13f'))
+            else:
+                altitude = altitude + float(format(random.random(), '.13f'))
             retset.append((step, (location['lat'], location['lng'], altitude), location['appears'], location['leaves']))
 
         return retset
